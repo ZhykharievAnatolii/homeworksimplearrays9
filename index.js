369478c00f5df27ec5783861a0a84254b9bc7d77
ZhykharievAnatolii <106694298+ZhykharievAnatolii@users.noreply.github.com>
2022-09-02 12:13:34 +0300

    add new files
    add new html js files

---
+++ index.js
@@ -0,0 +1,78 @@
+"use strict"
+const arrBookStore=[
+
+    bookShelfFirst
+{
+    name:`The Stranger`,
+    author :`Albert Camus`,
+    genre:`novella`,
+    year:1942,
+}
+    bookShelfSecond
+    {
+        name:`The Name of the Rose`,
+        author :`Umberto Eco`,
+        genre:`novella`,
+        year:1980,
+    }
+    bookShelfThird
+    {
+        name:`The Magic Mountain`,
+        author :`Thomas Mann`,
+        genre:`novella`,
+        year:1924,
+    }
+    bookShelfFourth
+    {
+        name:`The Second Sex`,
+        author :`Simone de Beauvoir`,
+        genre:`philosophy`,
+        year:1949,
+    }
+    bookShelfFifth
+    {
+        name:`The Blue Lotus`,
+        author :`Hergé`,
+        genre:`comics`,
+        year:1936,
+    }
+    bookShelfSixth
+    {
+        name:`Numero Zero`,
+        author :`Umberto Eco`,
+        genre:`novella`,
+        year:2015,
+    }
+    bookShelfSeventh
+    {
+        name:`The Prague Cemetery`,
+        author :`Umberto Eco`,
+        genre:`novella`,
+        year:2010,
+    }
+    ]
+
+};
+// store['clients']={
+//     NataliiaKulybina:{
+//         quantity:10,
+//         bonuses:20,
+//     },
+//     OleksiiMedvediev:{
+//         quantity:11,
+//         bonuses:21,
+//     },
+//     YuriyKruchinin:{
+//         quantity:12,
+//         bonuses:22,
+//     },
+//     MykolaPrymachok:{
+//         quantity:13,
+//         bonuses:23,
+//     },
+//     BohdanOnatskiy:{
+//         quantity:14,
+//         bonuses:24,
+//     },
+// };
+// console.log(store);
